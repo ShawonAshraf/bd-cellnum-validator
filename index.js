36a665f969d32a89e2249589499bed4be08ff007
Shawon Ashraf <shawon13@live.com>
2018-07-17 23:54:36 +0600

    comment and refactored param

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-var isValid = function(mobileNumber) {
-    const regex = /01(7|8|6|9|5)(\d){8}/;
-    var result = regex.exec(mobileNumber);
+/**
+ * Created by : Shawon Ashraf on 17-July-2018
+ * https: //github.com/ShawonAshraf/bd-cellnum-validator
+ */
 
-    if (result != null && result[0] === mobileNumber) {
+/**
+ * isValid function
+ * param - cellNumber to validate
+ * param type - String
+ * return value - a boolean value, returns true if the the param is valid and false otherwise 
+ */
+
+var isValid = function(cellNumber) {
+    const regex = /01(7|8|6|9|5)(\d){8}/;
+    var result = regex.exec(cellNumber);
+
+    if (result != null && result[0] === cellNumber) {
         return true;
     } else {
         return false;
     }
 }
 
+/**
+ * export module
+ */
 module.exports = {
     isValid
 }
