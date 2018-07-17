deb8f180c94517e13991d4d31d0e2d39396adeca
Shawon Ashraf <shawon13@live.com>
2018-07-17 23:13:18 +0600

    added validator.js

---
+++ validator.js
@@ -0,0 +1,12 @@
+var isValid = function(mobileNumber) {
+    const regex = /01(7|8|6|9|5)(\d){8}/;
+    var result = regex.exec(mobileNumber);
+
+    if (result != null && result[0] === mobileNumber) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+modules.export.isValid = isValid
