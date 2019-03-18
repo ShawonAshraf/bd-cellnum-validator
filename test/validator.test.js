e30d86fc99b9130ecb7680bfe32b72c0c8aa5c3e
Shawon Ashraf <shawon13@live.com>
2019-03-18 23:19:35 +0600

    updated test for 014

--- test/validator.test.js
+++ test/validator.test.js
@@ -1,8 +1,8 @@
 const validator = require('./../index');
 const expect = require('expect');
 
-describe('Validator test', function(done) {
-    it('Should validate a GP (017xxxx) number', function(done) {
+describe('Validator test', function (done) {
+    it('Should validate a GP (017xxxx) number', function (done) {
         var mobileNumber = '01711100022';
         var result = validator.isValid(mobileNumber);
 
@@ -10,15 +10,15 @@ describe('Validator test', function(done) {
         done();
     });
 
-  it('Should validate a GP (013xxxx) number', function(done) {
-    var mobileNumber = '01300000000';
-    var result = validator.isValid(mobileNumber);
+    it('Should validate a GP (013xxxx) number', function (done) {
+        var mobileNumber = '01300000000';
+        var result = validator.isValid(mobileNumber);
 
-    expect(result).toBe(true);
-    done();
-  });
+        expect(result).toBe(true);
+        done();
+    });
 
-    it("Should validate a Robi number", function(done) {
+    it("Should validate a Robi number", function (done) {
         var mobileNumber = '01800000000';
         var result = validator.isValid(mobileNumber);
 
@@ -26,7 +26,7 @@ describe('Validator test', function(done) {
         done();
     });
 
-    it("Should validate a Airtel number", function(done) {
+    it("Should validate a Airtel number", function (done) {
         var mobileNumber = '01600000000';
         var result = validator.isValid(mobileNumber);
 
@@ -34,7 +34,7 @@ describe('Validator test', function(done) {
         done();
     });
 
-    it("Should validate a BanglaLink number", function(done) {
+    it("Should validate a BanglaLink (019xxxx) number", function (done) {
         var mobileNumber = '01900000000';
         var result = validator.isValid(mobileNumber);
 
@@ -42,7 +42,15 @@ describe('Validator test', function(done) {
         done();
     });
 
-    it("Should validate a Teletalk number", function(done) {
+    it("Should validate a BanglaLink (014xxxx) number", function (done) {
+        var mobileNumber = '01400000000';
+        var result = validator.isValid(mobileNumber);
+
+        expect(result).toBe(true);
+        done();
+    });
+
+    it("Should validate a Teletalk number", function (done) {
         var mobileNumber = '01500000000';
         var result = validator.isValid(mobileNumber);
 
@@ -50,7 +58,7 @@ describe('Validator test', function(done) {
         done();
     });
 
-    it('Should reject an invalid number', function(done) {
+    it('Should reject an invalid number', function (done) {
         var mobileNumber = '122121';
         var result = validator.isValid(mobileNumber);
 
