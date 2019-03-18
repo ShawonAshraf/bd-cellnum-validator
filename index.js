5dd0fcdfa10089b70d40695eb15f6f8bf6f1f133
Shawon Ashraf <shawon13@live.com>
2019-03-18 23:16:22 +0600

    updated regex

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
  */
 
 var isValid = function(cellNumber) {
-    const regex = /(\+){0,1}(88){0,1}01(3|7|8|6|9|5)(\d){8}/;
+    const regex = /(((\+)|(00))88)?01(3|4|5|6|7|8|9)(\d){8}/;
     var result = regex.exec(cellNumber);
 
     if (result != null && result[0] === cellNumber) {
